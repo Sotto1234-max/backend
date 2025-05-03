4f5b5f0790d78ed564d53f2d156b4560ff8b1bc7
Sotto1234-max <mrsotto05@gmail.com>
2025-05-03 14:15:32 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,52 +1,49 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
 const path = require('path');
+const { Server } = require('socket.io');
 const { ExpressPeerServer } = require('peer');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
-
-// Setup PeerJS signaling server
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
 
 const peerServer = ExpressPeerServer(server, {
   debug: true,
-  path: '/peerjs'
+  path: "/peerjs"
 });
-app.use('/peerjs', peerServer); // must be before app.use(express.static(...))
 
+app.use("/peerjs", peerServer);
+app.use(express.static(path.join(__dirname, "public")));
 
-// Serve frontend
-app.use(express.static(path.join(__dirname, 'public')));
-
-// 1v1 matchmaking
 let waitingUser = null;
 
-io.on('connection', (socket) => {
-  console.log('User connected');
+io.on("connection", (socket) => {
+  console.log("User connected");
 
-  socket.on('join', (peerId) => {
+  socket.on("join", (peerId) => {
     socket.peerId = peerId;
 
     if (waitingUser) {
-      // Match found
       socket.partner = waitingUser;
       waitingUser.partner = socket;
 
-      waitingUser.emit('match', socket.peerId);
-      socket.emit('match', waitingUser.peerId);
+      socket.emit("match", waitingUser.peerId);
+      waitingUser.emit("match", socket.peerId);
 
       waitingUser = null;
     } else {
-      // No match yet, wait
       waitingUser = socket;
     }
   });
 
-  socket.on('leave', () => {
+  socket.on("leave", () => {
     if (socket.partner) {
-      socket.partner.emit('leave');
+      socket.partner.emit("leave");
       socket.partner.partner = null;
     }
     if (waitingUser === socket) {
@@ -55,9 +52,9 @@ io.on('connection', (socket) => {
     socket.partner = null;
   });
 
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     if (socket.partner) {
-      socket.partner.emit('leave');
+      socket.partner.emit("leave");
       socket.partner.partner = null;
     }
     if (waitingUser === socket) {
@@ -67,5 +64,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(3000, () => {
-  console.log('✅ Server running on http://localhost:3000');
+  console.log("✅ Server running on http://localhost:3000");
 });
